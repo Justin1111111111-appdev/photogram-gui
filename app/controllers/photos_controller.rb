e7268672bb8e31776442ab0fe8ecb8f5dbb4cc33
Justin1111111111 <stuwrkr-jguzm21@mx.uillinois.edu>
2023-02-28 18:00:36 +0000

    progress

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -32,4 +32,23 @@ the_photo.destroy
 
   redirect_to("/photos")
   end
+
+  def create 
+    #Parameters: {"query_image"=>"https://omsdpiprod.wpenginepowered.com/wp-content/uploads/2020/12/DPI_Aerial-OPT3-1-scaled-1-2048x1152.jpeg", "query_caption"=>"DPI", "query_owner_id"=>"117"}
+
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+
+  a_new_photo = Photo.new  
+
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+
+  a_new_photo.save
+
+    #render({ :template => "photo_templates/create.html.erb" })
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
 end
